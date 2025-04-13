d213147b146b864e0eb168fbf5f3ca13e392d5b5
Kojo Amfo-Baah <Kojo8baah@gmail.com>
2025-04-13 07:41:04 +0000

    Regenerate HTML when updating delivery option

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,6 +4,7 @@ import { formatCurrency } from "./utils/money.js";
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 import { deliveryOptions } from "../data/deliveryOptions.js";
 
+function renderOrderSummary(){
 let cartSummaryHTML = '';
 
 cart.forEach((cartItem) => {
@@ -127,5 +128,9 @@ document.querySelectorAll('.js-delivery-option')
   element.addEventListener('click',() => {
     const {productId,deliveryOptionId} = element.dataset;
     updateDeliveryOption(productId,deliveryOptionId);
+    renderOrderSummary();
   });
 });
+}
+
+renderOrderSummary();
